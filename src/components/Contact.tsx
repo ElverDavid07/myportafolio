7556ded9cf5446f085df76d23c958ee08f1b3c0b
Elver David <elverdavid0839@gmail.com>
2023-10-02 14:38:12 -0500

    dependencias actualizadas y responsive definitivo

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,138 +7,138 @@ import { useForm } from "react-hook-form";
 import { ToastContainer, Zoom, toast } from "react-toastify";
 
 const Contact = () => {
-	const [loaderBtn, setLoaderBtn] = useState(true);
-	//* Hook validate form
-	const {
-		register,
-		reset,
-		handleSubmit,
-		formState: { errors, isValid },
-	} = useForm<Data>();
-	//* Submit email
-	const submitData: SubmitHandler<Data> = async () => {
-		try {
-			await emailJs.sendForm(
-				"service_aqcg77r",
-				"template_9v5xeya",
-				"#formId",
-				"HmJktww9rFB4Doery",
-			);
-			reset();
-			toast.info("correo enviado correctamente");
-		} catch (error) {
-			console.log(error);
-		}
-		setLoaderBtn(true);
-	};
+  const [loaderBtn, setLoaderBtn] = useState(true);
+  //* Hook validate form
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<Data>();
+  //* Submit email
+  const submitData: SubmitHandler<Data> = async () => {
+    try {
+      await emailJs.sendForm(
+        "service_aqcg77r",
+        "template_9v5xeya",
+        "#formId",
+        "HmJktww9rFB4Doery"
+      );
+      reset();
+      toast.info("correo enviado correctamente");
+    } catch (error) {
+      console.log(error);
+    }
+    setLoaderBtn(true);
+  };
 
-	return (
-		<>
-			<h2 className="Subtitle text-center mt-32 mb-16" id="contact">
-				Contacto
-			</h2>
-			<p className="font-nunito text-slate-700 dark:text-gray-400 text-lg  mb-16 text-center">
-				¡Gracias por visitar mi portafolio! Si deseas ponerte en contacto
-				conmigo, puedes enviarme un correo electrónico, atravez de este
-				formulario.
-			</p>
+  return (
+    <>
+      <h2 className="Subtitle text-center mt-32 mb-16" id="contact">
+        Contacto
+      </h2>
+      <p className="font-nunito text-slate-700 dark:text-gray-400 text-lg  mb-16 text-center">
+        ¡Gracias por visitar mi portafolio! Si deseas ponerte en contacto
+        conmigo, puedes enviarme un correo electrónico, atravez de este
+        formulario.
+      </p>
 
-			{/* Form */}
-			<form className="Form" onSubmit={handleSubmit(submitData)} id="formId">
-				{/*Input Name  */}
-				<label htmlFor="nombre">Nombre</label>
-				<Input
-					isClearable
-					size="lg"
-					type="text"
-					placeholder="Ingresa tu nombre"
-					{...register("nombre", {
-						required: true,
-						minLength: 3,
-						maxLength: 50,
-					})}
-					variant="bordered"
-					validationState={errors.nombre ? "invalid" : "valid"}
-					errorMessage={
-						errors.nombre
-							? errors.nombre.type === "required"
-								? "Este campo es requerido!"
-								: errors.nombre.type === "minLength"
-								? "El nombre debe tener al menos 3 caracteres!"
-								: errors.nombre.type === "maxLength"
-								? "El nombre debe tener máximo 50 caracteres!"
-								: undefined
-							: undefined
-					}
-				/>
+      {/* Form */}
+      <form className="Form" onSubmit={handleSubmit(submitData)} id="formId">
+        {/*Input Name  */}
+        <label htmlFor="nombre">Nombre</label>
+        <Input
+          isClearable
+          size="lg"
+          type="text"
+          placeholder="Ingresa tu nombre"
+          {...register("nombre", {
+            required: true,
+            minLength: 3,
+            maxLength: 50,
+          })}
+          variant="bordered"
+          validationState={errors.nombre ? "invalid" : "valid"}
+          errorMessage={
+            errors.nombre
+              ? errors.nombre.type === "required"
+                ? "Este campo es requerido!"
+                : errors.nombre.type === "minLength"
+                ? "El nombre debe tener al menos 3 caracteres!"
+                : errors.nombre.type === "maxLength"
+                ? "El nombre debe tener máximo 50 caracteres!"
+                : undefined
+              : undefined
+          }
+        />
 
-				{/* Input Email */}
-				<label htmlFor="correo">Correo</label>
-				<Input
-					isClearable
-					size="lg"
-					variant="bordered"
-					validationState={errors.correo ? "invalid" : "valid"}
-					type="email"
-					placeholder="Ingresa tu correo electronico"
-					{...register("correo", {
-						required: true,
-						pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-					})}
-					errorMessage={
-						errors.correo
-							? errors.correo.type === "required"
-								? "Este campo es requerido!"
-								: errors.correo.type === "pattern"
-								? "Escribe un correo valido!"
-								: undefined
-							: undefined
-					}
-				/>
+        {/* Input Email */}
+        <label htmlFor="correo">Correo</label>
+        <Input
+          isClearable
+          size="lg"
+          variant="bordered"
+          validationState={errors.correo ? "invalid" : "valid"}
+          type="email"
+          placeholder="Ingresa tu correo electronico"
+          {...register("correo", {
+            required: true,
+            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+          })}
+          errorMessage={
+            errors.correo
+              ? errors.correo.type === "required"
+                ? "Este campo es requerido!"
+                : errors.correo.type === "pattern"
+                ? "Escribe un correo valido!"
+                : undefined
+              : undefined
+          }
+        />
 
-				{/* Input Message */}
-				<label htmlFor="message">Mensaje</label>
-				<Textarea
-					variant="bordered"
-					size="lg"
-					validationState={errors.mensaje ? "invalid" : "valid"}
-					placeholder="Escribe tu mensaje aqui"
-					{...register("mensaje", { required: true })}
-					className="resize-none"
-					errorMessage={
-						errors.mensaje
-							? errors.mensaje.type === "required"
-								? "Este campo es requerido!"
-								: undefined
-							: undefined
-					}
-				/>
+        {/* Input Message */}
+        <label htmlFor="message">Mensaje</label>
+        <Textarea
+          variant="bordered"
+          size="lg"
+          validationState={errors.mensaje ? "invalid" : "valid"}
+          placeholder="Escribe tu mensaje aqui"
+          {...register("mensaje", { required: true })}
+          className="resize-none"
+          errorMessage={
+            errors.mensaje
+              ? errors.mensaje.type === "required"
+                ? "Este campo es requerido!"
+                : undefined
+              : undefined
+          }
+        />
 
-				<Button
-					radius="full"
-					variant="ghost"
-					color="primary"
-					type="submit"
-					onClick={() => {
-						isValid === true ? setLoaderBtn(false) : "";
-					}}
-				>
-					{loaderBtn === false ? (
-						<Spinner aria-label="spinner" color="white" />
-					) : (
-						"Enviar"
-					)}
-				</Button>
-			</form>
-			<ToastContainer
-				autoClose={1000}
-				closeOnClick={false}
-				closeButton={false}
-				pauseOnHover={false}
-				transition={Zoom}
-			/>
-		</>
-	);
+        <Button
+          radius="full"
+          variant="ghost"
+          color="primary"
+          type="submit"
+          onClick={() => {
+            isValid === true ? setLoaderBtn(false) : "";
+          }}
+        >
+          {loaderBtn === false ? (
+            <Spinner aria-label="spinner" color="white" />
+          ) : (
+            "Enviar"
+          )}
+        </Button>
+      </form>
+      <ToastContainer
+        autoClose={1000}
+        closeOnClick={false}
+        closeButton={false}
+        pauseOnHover={false}
+        transition={Zoom}
+      />
+    </>
+  );
 };
 
 export default Contact;
